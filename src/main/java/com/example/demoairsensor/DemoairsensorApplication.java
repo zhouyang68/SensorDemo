3112ac774ce6abe157b83f20d3c8581556108217
Zhou Yang <yang0190@gmail.com>
2019-04-24 11:08:06 -0700

    first commit

---
+++ src/main/java/com/example/demoairsensor/DemoairsensorApplication.java
@@ -0,0 +1,13 @@
+package com.example.demoairsensor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoairsensorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoairsensorApplication.class, args);
+    }
+
+}
